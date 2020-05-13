83827824b8019df11dd414aa837ff9763de29c5d
chauhm2912 <62837484+chauhm2912@users.noreply.github.com>
2020-05-13 14:51:13 +0700

    Add files via upload

---
+++ file_scan.py
@@ -0,0 +1,21 @@
+import time
+
+start_time = time.time()
+
+file = open('E:\Chau\Ki8\CTPM_HTTG_Thuc\pg100.txt', "r")
+wordcount = {}
+for word in file.read().split():
+	if word not in wordcount:
+		wordcount[word] = 1
+	else:
+		wordcount[word] += 1
+
+for key in wordcount.keys():
+	print(key, '', wordcount[key])
+
+end_time = time.time()
+
+print()
+print((end_time - start_time)*1000, "ms")
+
+file.close()
